6e641e527d81023f03d3ea8bca79b2f5edadeb17
Alex Spacek <aspck@outlook.com>
2021-01-26 01:01:24 -0800

    initial commit of desktop app with basic UI functions

---
+++ software/desktop-config-GUI/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWtest
+{    
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+ 
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            NavigationViewItem i = sender.SelectedItem as NavigationViewItem;
+            switch (i.Name)
+            {
+                case "connectpage":
+                    MainFrame.Navigate(typeof(ConnectPage));
+                    break;
+
+                case "keyboardpage":
+                    MainFrame.Navigate(typeof(KeyboardPage));
+                    break;
+
+   
+
+            }
+        }
+    }
+
+
+}
