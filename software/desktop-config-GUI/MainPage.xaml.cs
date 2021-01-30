aee3f3c61e04f3477a1563041d98358122a85258
Alex Spacek <aspck@outlook.com>
2021-01-29 23:33:17 -0800

    implemented profile read/write system

--- software/desktop-config-GUI/MainPage.xaml.cs
+++ software/desktop-config-GUI/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Search;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,6 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Diagnostics;
+using System.Xml;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,140 +26,147 @@ namespace UWtest
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public Dictionary<string, int> KeyList { get; } = new Dictionary<string, int>() {
-                {"A", 0x04 },
-                {"B", 0x05 },
-                {"C", 0x06 },
-                {"D", 0x07 },
-                {"E", 0x08 },
-                {"F", 0x09 },
-                {"G", 0x0a },
-                {"H", 0x0b },
-                {"I", 0x0c },
-                {"J", 0x0d },
-                {"K", 0x0e },
-                {"L", 0x0f },
-                {"M", 0x10 },
-                {"N", 0x11 },
-                {"O", 0x12 },
-                {"P", 0x13 },
-                {"Q", 0x14 },
-                {"R", 0x15 },
-                {"S", 0x16 },
-                {"T", 0x17 },
-                {"U", 0x18 },
-                {"V", 0x19 },
-                {"W", 0x1a },
-                {"X", 0x1b },
-                {"Y", 0x1c },
-                {"Z", 0x1d },
-                {"1", 0x1e },
-                {"2", 0x1f },
-                {"3", 0x20 },
-                {"4", 0x21 },
-                {"5", 0x22 },
-                {"6", 0x23 },
-                {"7", 0x24 },
-                {"8", 0x25 },
-                {"9", 0x26 },
-                {"0", 0x27 },
-                {"ENTER", 0x28 },
-                {"ESC", 0x29 },
-                {"BACK\nSPACE", 0x2a },
-                {"TAB", 0x2b },
-                {"SPACE", 0x2c },
-                {"LEFT\nCTRL", 0xe0 },
-                {"LEFT\nSHIFT", 0xe1 },
-                {"LEFT\nALT", 0xe2 },
-                {"LEFT\nMETA", 0xe3 },
-                {"RIGHT\nCTRL", 0xe4 },
-                {"RIGHT\nSHIFT", 0xe5 },
-                {"RIGHT\nALT", 0xe6 },
-                {"RIGHT\nMETA", 0xe7 },
-                {"-", 0x2d },
-                {"=", 0x2e },
-                {"[", 0x2f },
-                {"]", 0x30 },
-                {"\\", 0x31 },
-                {"`", 0x32 },
-                {";", 0x33 },
-                {"'", 0x34 },
-                {"GRAVE", 0x35 },
-                {",", 0x36 },
-                {".", 0x37 },
-                {"/", 0x38 },
-
-                {"F1", 0x3a },
-                {"F2", 0x3b },
-                {"F3", 0x3c },
-                {"F4", 0x3d },
-                {"F5", 0x3e },
-                {"F6", 0x3f },
-                {"F7", 0x40 },
-                {"F8", 0x41 },
-                {"F9", 0x42 },
-                {"F10", 0x43 },
-                {"F11", 0x44 },
-                {"F12", 0x45 },
-                {"SYSRQ", 0x46 },
-                {"SCROLL\nLOCK", 0x47 },
-                {"NUM\nLOCK", 0x53 },
-                {"CAPS\nLOCK", 0x39 },
-                {"PAUSE", 0x48 },
-                {"INSERT", 0x49 },
-                {"HOME", 0x4a },
-                {"PAGEUP", 0x4b },
-                {"DELETE", 0x4c },
-                {"END", 0x4d },
-                {"PAGE\nDOWN", 0x4e },
-                {"RIGHT", 0x4f },
-                {"LEFT", 0x50 },
-                {"DOWN", 0x51 },
-                {"UP", 0x52 },
-                {"MUTE", 0x7f },
-                {"VOL\nUP", 0x80 },
-                {"VOL\nDOWN", 0x81 },
-                {"KP\n\\", 0x54 },
-                {"KP\n*", 0x55 },
-                {"KP\n-", 0x56 },
-                {"KP\n+", 0x57 },
-                {"KP\nENTER", 0x58 },
-                {"KP1", 0x59 },
-                {"KP2", 0x5a },
-                {"KP3", 0x5b },
-                {"KP4", 0x5c },
-                {"KP5", 0x5d },
-                {"KP6", 0x5e },
-                {"KP7", 0x5f },
-                {"KP8", 0x60 },
-                {"KP9", 0x61 },
-                {"KP0", 0x62 },
-                {"KP\n.", 0x63 }
-                //{"OPEN", 0x74 },
-                //{"HELP", 0x75 },
-                //{"PROPS", 0x76 },
-                //{"FRONT", 0x77 },
-                //{"STOP", 0x78 },
-                //{"AGAIN", 0x79 },
-                //{"UNDO", 0x7a },
-                //{"CUT", 0x7b },
-                //{"COPY", 0x7c },
-                //{"PASTE", 0x7d },
-                //{"FIND", 0x7e },
+        private Dictionary<byte, string> KeyList { get; } = new Dictionary<byte, string>() {
+  { 0x4, "A" },
+{ 0x5, "B" },
+{ 0x6, "C" },
+{ 0x7, "D" },
+{ 0x8, "E" },
+{ 0x9, "F" },
+{ 0xA, "G" },
+{ 0xB, "H" },
+{ 0xC, "I" },
+{ 0xD, "J" },
+{ 0xE, "K" },
+{ 0xF, "L" },
+{ 0x10, "M" },
+{ 0x11, "N" },
+{ 0x12, "O" },
+{ 0x13, "P" },
+{ 0x14, "Q" },
+{ 0x15, "R" },
+{ 0x16, "S" },
+{ 0x17, "T" },
+{ 0x18, "U" },
+{ 0x19, "V" },
+{ 0x1A, "W" },
+{ 0x1B, "X" },
+{ 0x1C, "Y" },
+{ 0x1D, "Z" },
+{ 0x1E, "1" },
+{ 0x1F, "2" },
+{ 0x20, "3" },
+{ 0x21, "4" },
+{ 0x22, "5" },
+{ 0x23, "6" },
+{ 0x24, "7" },
+{ 0x25, "8" },
+{ 0x26, "9" },
+{ 0x27, "0" },
+{ 0x28, "ENTER" },
+{ 0x29, "ESC" },
+{ 0x2A, "BACK SPACE" },
+{ 0x2B, "TAB" },
+{ 0x2C, "SPACE" },
+{ 0xE0, "LEFT CTRL" },
+{ 0xE1, "LEFT SHIFT" },
+{ 0xE2, "LEFT ALT" },
+{ 0xE3, "LEFT META" },
+{ 0xE4, "RIGHT CTRL" },
+{ 0xE5, "RIGHT SHIFT" },
+{ 0xE6, "RIGHT ALT" },
+{ 0xE7, "RIGHT META" },
+{ 0x2D, "-" },
+{ 0x2E, "=" },
+{ 0x2F, "[" },
+{ 0x30, "]" },
+{ 0x31, "\\" },
+{ 0x32, "`" },
+{ 0x33, ";" },
+{ 0x34, "'" },
+{ 0x35, "GRAVE" },
+{ 0x36, "," },
+{ 0x37, "." },
+{ 0x38, "/" },
+{ 0x3A, "F1" },
+{ 0x3B, "F2" },
+{ 0x3C, "F3" },
+{ 0x3D, "F4" },
+{ 0x3E, "F5" },
+{ 0x3F, "F6" },
+{ 0x40, "F7" },
+{ 0x41, "F8" },
+{ 0x42, "F9" },
+{ 0x43, "F10" },
+{ 0x44, "F11" },
+{ 0x45, "F12" },
+{ 0x46, "SYSRQ" },
+{ 0x47, "SCROLL LOCK" },
+{ 0x53, "NUM LOCK" },
+{ 0x39, "CAPS LOCK" },
+{ 0x48, "PAUSE" },
+{ 0x49, "INSERT" },
+{ 0x4A, "HOME" },
+{ 0x4B, "PAGEUP" },
+{ 0x4C, "DELETE" },
+{ 0x4D, "END" },
+{ 0x4E, "PAGE DOWN" },
+{ 0x4F, "RIGHT" },
+{ 0x50, "LEFT" },
+{ 0x51, "DOWN" },
+{ 0x52, "UP" },
+{ 0x7F, "MUTE" },
+{ 0x80, "VOL UP" },
+{ 0x81, "VOL DOWN" },
+{ 0x54, "KP \\" },
+{ 0x55, "KP *" },
+{ 0x56, "KP -" },
+{ 0x57, "KP +" },
+{ 0x58, "KP ENTER" },
+{ 0x59, "KP1" },
+{ 0x5A, "KP2" },
+{ 0x5B, "KP3" },
+{ 0x5C, "KP4" },
+{ 0x5D, "KP5" },
+{ 0x5E, "KP6" },
+{ 0x5F, "KP7" },
+{ 0x60, "KP8" },
+{ 0x61, "KP9" },
+{ 0x62, "KP0" },
+{ 0x63, "KP ." },
 
 };
-        public List<ComboBox> LeftHKeys = new List<ComboBox>();
-        public List<ComboBox> RightHKeys = new List<ComboBox>();
+        private List<ComboBox> LeftHKeys = new List<ComboBox>();
+        private List<ComboBox> RightHKeys = new List<ComboBox>();
+
+        private int nKeys = 50;  // 25 per device
+        private int KeyReportSize = 13;
+
+        private string Characteristic1UUID = "6AF7ACFD-F66B-4932-8975-41F512990077";
+        private string Characteristic2UUID = "4F229A78-0344-430A-B203-CF8685E77E2C";
+
+        // 10A050-FAFF01
+        //private long DeviceAddressLeft = 0x01fffa50a010;
+
 
         public MainPage()
         {
+            // load app config file            
+
+
             this.InitializeComponent();
-            
+
+            CBProfiles_RefreshFiles(CBProfiles);
+
+            // get all key selection controls
             FindChildren<ComboBox>(LeftHKeys, leftkeymapcontainer, "CBKey");            
             FindChildren<ComboBox>(RightHKeys, rightkeymapcontainer, "CBKey");
 
-        }
+            // set default profiles
+            //CBProfiles.ItemsSource
+            // 
 
+        }
 
         internal static void FindChildren<T>(List<T> results, DependencyObject startNode, string Tag)
 where T : DependencyObject
@@ -178,16 +187,162 @@ where T : DependencyObject
                 FindChildren<T>(results, current, Tag);
             }
         }
+        
+        internal void ParseKeys(byte[] storageBuffer, List<ComboBox> L, List<ComboBox> R)
+        {
+            int i = 0;
+            for (; i < L.Count; i++)
+            {
+                storageBuffer[i] = (byte)L[i].SelectedValue;
+            }
+            for (int j = 0; j <R.Count; j++)
+            {
+                storageBuffer[i+j] = (byte)R[j].SelectedValue;
+            }
+        }
 
-        private void CBProfiles_DropDownOpened(object sender, object e)
+        internal async void CBProfiles_RefreshFiles(object _combobox)
         {
             // get config files in directory
-            List<string> xmlfiles = new List<string>();
-
-
-            // update item source with file list
-            (sender as ComboBox).ItemsSource = xmlfiles;
+            IReadOnlyList<Windows.Storage.StorageFile> xmlFiles = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileQueryWithOptions(
+                new QueryOptions(CommonFileQuery.OrderByName, new List<string>() { ".keyprofile" })).GetFilesAsync();
+            
+            if (xmlFiles != null)
+            {
+                // list of filenames for combobox
+                ObservableCollection<string> xmlList = new ObservableCollection<string>(xmlFiles.Select(p => p.DisplayName).ToList());
+                // add new profile action to end of list
+                xmlList.Add("< Create new profile >");
+                // update item source with file list
+                (_combobox as ComboBox).ItemsSource = xmlList;            
+            }      
+            
         }
+        
+        private void CBProfiles_DropDownOpened(object sender, object e)
+        {
+        }
+        
+        private void CBProfiles_DropDownClosed(object sender, object e)
+        {
+            var s = (sender as ComboBox).SelectedItem;
+            // just handles the "create new profile" action
+            if (s != null && s.ToString() == "< Create new profile >"){
+                // show popup with text dialog
+                if (!NewProfilePopup.IsOpen) { NewProfilePopup.IsOpen = true; }
+            }
+
+        }
+       
+        private async void CreateProfilePopupClicked(object sender, RoutedEventArgs e)
+        {
+            string fname = CreateProfileText.Text;
+
+            // try to create file
+            Windows.Storage.StorageFolder storageFolder =
+                Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            Windows.Storage.StorageFile newFile =
+                await storageFolder.CreateFileAsync(fname + ".keyprofile", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+ 
+            // write keys to file            
+
+            // update combobox selection
+            CBProfiles_RefreshFiles(CBProfiles);
+            CBProfiles.SelectedItem = fname;
+
+            // if the Popup is open, then close it 
+            if (NewProfilePopup.IsOpen) { NewProfilePopup.IsOpen = false; }
+
+        }
+        
+        private void CancelProfilePopupClicked(object sender, RoutedEventArgs e)
+        {
+            CBProfiles.SelectedItem = null;
+            // if the Popup is open, then close it 
+            if (NewProfilePopup.IsOpen) { NewProfilePopup.IsOpen = false; }
+        }
+        
+        private async void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // try to open file
+            try
+            {
+                if (CBProfiles.SelectedValue == null) { return; }
+                var fstream = await Windows.Storage.ApplicationData.Current.LocalFolder.OpenStreamForReadAsync( 
+                    CBProfiles.SelectedValue.ToString() + ".keyprofile");
+                byte[] keyBuffer = new byte[nKeys];
+
+                // read key array
+                fstream.Read(keyBuffer, 0, nKeys);
+                await fstream.FlushAsync();
+                fstream.Dispose();
+
+
+                // if valid, update ui controls
+                int i = 0;
+                for (; i < LeftHKeys.Count; i++)
+                {
+                    // attempts to select the item in the combobox itemssource that matches the int
+                    LeftHKeys[i].SelectedValue = keyBuffer[i];
+                }
+                for (int j = 0; j < RightHKeys.Count; j++)
+                {
+                    RightHKeys[j].SelectedValue = keyBuffer[i+j];
+                }
+
+            } // not sure what to do with these
+            catch (NullReferenceException)
+            {
+
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            catch (IOException)
+            {
+
+            }
+
+
+        }
+        
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // try to create backup of current profile
+
+
+            try
+            {
+                if (CBProfiles.SelectedValue == null) { return; }
+                var fstream = await Windows.Storage.ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync( 
+                    CBProfiles.SelectedValue.ToString() + ".keyprofile", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+                byte[] keyBuffer = new byte[nKeys];
+
+                // get selected keys
+                ParseKeys(keyBuffer, LeftHKeys, RightHKeys);
+
+                // write keys to file
+                fstream.Write(keyBuffer, 0, nKeys);
+                await fstream.FlushAsync();
+                fstream.Dispose();
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+            catch (IOException)
+            {
+
+            }
+
+        }
+        
         private void UploadButton_Click(object sender, RoutedEventArgs e)
         {
             string errormsg;
@@ -206,15 +361,16 @@ where T : DependencyObject
             }
             // check device
             /// if (device is paired)
-                // write config
-                /// errormsg = "config uploaded to device"
+            // write config
+            /// errormsg = "config uploaded to device"
             /// else
-                /// errormsg = "upload failed: device not found"
+            /// errormsg = "upload failed: device not found"
 
             // display result of operation
             Popup popup = new Popup();
 
         }
+        
         private void DownloadButton_Click(object sender, RoutedEventArgs e)
         {
             string errormsg;
@@ -233,44 +389,15 @@ where T : DependencyObject
             }
             // check device
             /// if (device is paired)
-                /// read config and udpate keys
+            /// read config and udpate keys
             /// else
-                /// display error
-                Popup popup = new Popup();
+            /// display error
+            Popup popup = new Popup();
 
 
 
         }
-        private void LoadButton_Click(object sender, RoutedEventArgs e)
-        {
-            // try to open file
-
-            // read key array
-
-            // if valid, update ui controls
-
-        }
-
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-            // try to create backup of current profile
-
-            // write keys to file
-        }
-
-        private void NewButton_Click(object sender, RoutedEventArgs e)
-        {
-            // open text dialog
-
-            // try to create file
-
-            // write keys to file
-            
-            // add new path to list
-
-            // update combobox selection
-
-        }
+    
     }
 
 }
